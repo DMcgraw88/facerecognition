61ca7d194eb46d43a480744119ebc9dc70ce6ab7
DMcgraw88 <darrylmcgraw5@gmail.com>
2023-05-23 20:56:09 -0500

    created initial components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
 import Navigation from './Components/Navigation/Navigation'
+import Logo from './Components/Logo/Logo';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank';
 import 'tachyons';
 import './App.css';
+import './index.css';
+import Particles from "react-particles";
+
 
 function App() {
   return (
     <div className="App">
      <Navigation />
-
-      {/* <Logo />
-      <ImageLinkForm />
-      <FaceRecognition /> */}
-
+     <Logo />
+     <Rank/>
+     <ImageLinkForm/>
     </div>
   );
 }
