8ef94e285edf7f5ce61bed694238fa4061dbe66e
DMcgraw88 <darrylmcgraw5@gmail.com>
2023-07-16 13:21:32 -0500

    fixed bug where image perisited after logging out and back in due to state

--- src/App.js
+++ src/App.js
@@ -47,23 +47,26 @@ const returnClarifaiRequestOptions = (imageUrl) => {
 };
 }
 
+const initialState = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
+}
+
+
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
-      route: 'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''
-      }
-    }
+    this.state =  initialState;
   }
 
   loadUser = (data) => {
@@ -116,6 +119,7 @@ displayFaceBox = (box) => {
           .then(count => {
             this.setState(Object.assign(this.state.user, { entries: count}))
             })
+            .catch(console.log)
         }
         this.displayFaceBox(this.calculateFaceLocation(response))
       })
@@ -125,7 +129,7 @@ displayFaceBox = (box) => {
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     } else if (route === 'home'){
       this.setState({isSignedIn: true})
     }
